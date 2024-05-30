f78dad4646d71af6ec1baa67185c15c27a056215
Srija713 <srijarao2277@gmail.com>
2024-05-31 01:23:04 +0530

    modified to display payment and principal for every month

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,18 +6,21 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 months=0
-extra_payment_start_month = 0
-extra_payment_end_month = 12
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000
 
 while principal > 0:
     if months==extra_payment_start_month:
-        for i in range(extra_payment_end_month-extra_payment_start_month):
-            principal = principal * (1+rate/12) - (payment+extra_payment)
-            total_paid = total_paid + payment+extra_payment
+        for i in range(extra_payment_start_month,extra_payment_end_month+1):
+            principal = round(principal * (1+rate/12) - (payment+extra_payment),2)
+            total_paid = round(total_paid + payment+extra_payment,2)
             months=months+1
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+            print(months,total_paid,principal)
+    principal = round(principal * (1+rate/12) - payment,2)
+    total_paid = round(total_paid + payment,2)
     months=months+1
+    print(months,total_paid,principal)
 
-print('Total paid', total_paid,'months:',months)
+print('Total paid', total_paid)
+print('months',months)
