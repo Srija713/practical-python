df91bd2ec6a9ffec5601e32d81f57004673b450d
Srija713 <srijarao2277@gmail.com>
2024-06-12 22:11:55 +0530

    added comments to add contect

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,11 +2,12 @@
 #
 # Exercise 1.27
 import csv
+# a csv(comma separated value) file is a txt file which has data separated by commas
 def portfolio_cost(filename):
     cost=0
     f=open(filename)
-    rows=csv.reader(f)
-    header=next(rows)
+    rows=csv.reader(f) #converts each line into a list(works like split(,))
+    header=next(rows) #reads the upcoming line in this case first line
     for li in rows:
         cost=cost+(float(li[1])*float(li[2]))
     f.close()
