029804305be1906b7dec26118506eb1370661a18
Srija713 <srijarao2277@gmail.com>
2024-06-12 21:53:39 +0530

    using functions to return the cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
-cost=0
-with open('Data/portfolio.csv', 'rt') as f:
-    header=next(f)
-    for line in f:
-        li=line.split(',')
-        cost=cost+(float(li[1])*float(li[2]))
-print("Total cost",cost)
-f.close()
-
+def portfolio_cost(filename):
+    cost=0
+    with open(filename, 'rt') as f:
+        header=next(f)
+        for line in f:
+            li=line.split(',')
+            cost=cost+(float(li[1])*float(li[2]))
+    f.close()
+    return cost
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
