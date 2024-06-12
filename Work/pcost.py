2b7b35937e5c513426d4aa52d9ff86fa58162187
Srija713 <srijarao2277@gmail.com>
2024-06-12 18:25:12 +0530

    completed pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+cost=0
+with open('Data/portfolio.csv', 'rt') as f:
+    header=next(f)
+    for line in f:
+        li=line.split(',')
+        cost=cost+(float(li[1])*float(li[2]))
+print("Total cost",cost)
+f.close()
+
