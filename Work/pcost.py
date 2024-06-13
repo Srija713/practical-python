ecc4256cc9fbcc419cfcabe806db551766a5750f
Srija713 <srijarao2277@gmail.com>
2024-06-13 18:08:16 +0530

    modified pcost.py to take filename through command line arguments

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,14 +3,19 @@
 # Exercise 1.27
 import csv
 # a csv(comma separated value) file is a txt file which has data separated by commas
+import sys #used to read command line arguments
 def portfolio_cost(filename):
     cost=0
-    f=open(filename)
-    rows=csv.reader(f) #converts each line into a list(works like split(,))
-    header=next(rows) #reads the upcoming line in this case first line
-    for li in rows:
+    f=open(filename,'rt') 
+    header=next(f) 
+    for row in f:
+        li=row.split(',')
         cost=cost+(float(li[1])*float(li[2]))
     f.close()
     return cost
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+else:
+    filename='Data/portfolio.csv'
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
